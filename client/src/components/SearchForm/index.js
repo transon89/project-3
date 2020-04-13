e335731ab336a04ac2991c4eea81b316f6a71f98
Son <transon89@gmail.com>
2020-04-13 12:25:04 -0500

    server.js

---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,23 @@
+  
+import React from 'react';
+
+function SearchForm(props) {
+
+    return (
+        <div id='search-form' className='text-center'>
+            <input className='form-control' name='title'
+                placeholder='Search for a book...'
+                type='text'
+                onChange={props.handleInputChange}></input>
+                <br />
+                <button
+                className='btn btn-block btn-primary'
+                    onClick={props.handleFormSubmit}
+                    type='submit'
+                >   Search
+            </button>
+        </div>
+    )
+}
+
+export default SearchForm
--- server.js
+++ server.js
@@ -1,39 +1,32 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require ('mongoose')
 const routes = require('./routes')
 const fs = require('fs')
 
-// Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Define API routes here
+
 app.use(routes)
 
-// Send every other request to the React app
-// Define any API routes before this runs
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.createConnection(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",{
- useNewUrlParser: true, 
- useUnifiedTopology: true, 
- useCreateIndex: true,
- useFindAndModify: false
-});
-console.log("MongoDB Connected")
-
- 
-
-
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
+})
+  mongoose.connect("mongodb://localhost/googlebooks",{
+    useNewUrlParser: true 
+
 });
+console.log("MongoDB Connected")
